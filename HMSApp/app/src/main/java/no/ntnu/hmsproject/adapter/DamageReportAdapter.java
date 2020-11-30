0dbf6ce608fa7e611b99bd02f847239f9fd2ab55
Erlend Solbakken Nikolaisen <erls@stud.ntnu.no>
2020-11-30 15:17:02 +0100

    more recyclreview

--- HMSApp/app/src/main/java/no/ntnu/hmsproject/adapter/DamageReportAdapter.java
+++ HMSApp/app/src/main/java/no/ntnu/hmsproject/adapter/DamageReportAdapter.java
@@ -6,52 +6,63 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import no.ntnu.hmsproject.R;
+import no.ntnu.hmsproject.domain.DamageReport;
 
 public class DamageReportAdapter extends RecyclerView.Adapter<DamageReportAdapter.DamageReportViewHolder> {
-    List<DamageReport> 
+    List<DamageReport> damageReportList;
 
     public DamageReportAdapter() {
-
+        this.damageReportList = new ArrayList<>();
     }
 
     @NonNull
     @Override
-    public DamageReportViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
-                                             int viewType) {
-        ConstraintLayout layout = (ConstraintLayout) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.fragment_dam_report_list,
-                        parent,
-                        false);
-
-        DamageReportViewHolder viewHolder = new DamageReportViewHolder(layout);
-        return viewHolder;
+    public DamageReportViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_damrep, parent, false);
+        return new DamageReportViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull DamageReportViewHolder holder, int position) {
-
-
-    }
+        holder.damageReportId.setText(damageReportList.get(position).getBookingId());
+        holder.damageReportTitle.setText(damageReportList.get(position).getDamageTitle());
+        }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return damageReportList.size();
+    }
+
+    public void setDamageReportList(ArrayList<DamageReport> list) {
+        this.damageReportList = list;
+        notifyDataSetChanged();
     }
 
     class DamageReportViewHolder extends RecyclerView.ViewHolder {
         View view;
         TextView damageReportId;
-        TextView damageeReportTitle;
+        TextView damageReportTitle;
 
         public DamageReportViewHolder(@NonNull View view) {
             super(view);
-            view.setOnClickListener (v -> onClick(getAdapterPosition()));
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
             this.damageReportId = view.findViewById(R.id.damageReportId);
-            this.damageeReportTitle = view.findViewById(R.id.damageReportTitle);
+            this.damageReportTitle = view.findViewById(R.id.damageReportTitle);
         }
     }
 }
+
+
+
--- HMSApp/app/src/main/java/no/ntnu/hmsproject/ui/hmsservice/damagereport/FragmentDamReportList.java
+++ HMSApp/app/src/main/java/no/ntnu/hmsproject/ui/hmsservice/damagereport/FragmentDamReportList.java
@@ -3,13 +3,30 @@ package no.ntnu.hmsproject.ui.hmsservice.damagereport;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 import no.ntnu.hmsproject.R;
+import no.ntnu.hmsproject.adapter.DamageReportAdapter;
+import no.ntnu.hmsproject.domain.DamageReport;
 
 public class FragmentDamReportList extends Fragment {
+    private RecyclerView recyclerView;
+    private RelativeLayout relativeLayout;
+    private ArrayList<DamageReport> damageReports = new ArrayList<>();
+    private DamageReportAdapter damageReportAdapter;
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_dam_report_list, container, false);
+
+        recyclerView = view.findViewById(R.id.recycler_view_dam_report);
+        
+    }
 }
