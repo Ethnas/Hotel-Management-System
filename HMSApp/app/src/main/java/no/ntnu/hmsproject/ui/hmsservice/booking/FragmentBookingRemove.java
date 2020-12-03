f409b7dd544509847f840c6c7e12c95b17e8e6c5
Pål Nyseth <paalnyseth@gmail.com>
2020-12-03 19:14:18 +0100

    added missing nav, hope it fix other missing nav

--- HMSApp/app/src/main/java/no/ntnu/hmsproject/ui/hmsservice/booking/FragmentBookingRemove.java
+++ HMSApp/app/src/main/java/no/ntnu/hmsproject/ui/hmsservice/booking/FragmentBookingRemove.java
@@ -7,6 +7,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -82,6 +84,8 @@ public class FragmentBookingRemove extends Fragment implements AdapterView.OnIte
         StringRequest stringRequest = new StringRequest(
                 Request.Method.DELETE, url,
                 response -> {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+                navController.popBackStack();
                     System.out.println(response);
                     System.out.println("Things went smooth");
                 },
